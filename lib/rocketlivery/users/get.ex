c35cb0da3b8873341a4f6cd77afbeadbc113d0a5
Francisross Soares de Oliveira <franciscpd@gmail.com>
2022-04-03 15:55:27 -0300

    refactor: add module error build

--- lib/rocketlivery/users/get.ex
+++ lib/rocketlivery/users/get.ex
@@ -1,6 +1,6 @@
 defmodule Rocketlivery.Users.Get do
   alias Ecto.UUID
-  alias Rocketlivery.{Repo, User}
+  alias Rocketlivery.{Error, Repo, User}
 
   def all do
     Repo.all(User)
@@ -8,14 +8,14 @@ defmodule Rocketlivery.Users.Get do
 
   def by_id(id) do
     case UUID.cast(id) do
-      :error -> {:error, %{status: :bad_request, result: "Invalid id format"}}
+      :error -> {:error, Error.build_id_format_error()}
       {:ok, uuid} -> get(uuid)
     end
   end
 
   defp get(id) do
     case Repo.get(User, id) do
-      nil -> {:error, %{status: :not_found, result: "User not found"}}
+      nil -> {:error, Error.build_user_not_found_error()}
       user -> {:ok, user}
     end
   end
