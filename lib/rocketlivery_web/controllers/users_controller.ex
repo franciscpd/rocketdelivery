3326ce5af006d42650addbac9b3170717b97f6e2
Francisross Soares de Oliveira <franciscpd@gmail.com>
2022-04-03 15:43:28 -0300

    feat: add delete user

--- lib/rocketlivery_web/controllers/users_controller.ex
+++ lib/rocketlivery_web/controllers/users_controller.ex
@@ -20,8 +20,16 @@ defmodule RocketliveryWeb.UsersController do
     end
   end
 
+  def delete(conn, %{"id" => id}) do
+    with {:ok, %User{}} <- Rocketlivery.delete_user(id) do
+      conn
+      |> put_status(:no_content)
+      |> text("")
+    end
+  end
+
   def show(conn, %{"id" => id}) do
-    with {:ok, user} <- Rocketlivery.get_user_by_id(id) do
+    with {:ok, %User{} = user} <- Rocketlivery.get_user_by_id(id) do
       conn
       |> put_status(:ok)
       |> render("show.json", user: user)
