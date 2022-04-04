3db4c10802e07145c7a61d15ca8533b8c00a7378
Francisross Soares de Oliveira <franciscpd@gmail.com>
2022-04-03 21:32:07 -0300

    feat: tests user views

---
+++ test/rocketlivery_web/views/users_view_test.exs
@@ -0,0 +1,53 @@
+defmodule RocketliveryWeb.UsersViewTest do
+  use RocketliveryWeb.ConnCase, async: true
+
+  alias RocketliveryWeb.UsersView
+
+  import Phoenix.View
+  import Rocketlivery.Factory
+
+  test "renders create.json" do
+    user = build(:user)
+
+    response = render(UsersView, "create.json", user: user)
+
+    assert %{
+             message: "User created successfully",
+             user: %Rocketlivery.User{
+               address: "Rua Canto do Vaqueiro, 943",
+               age: 26,
+               cep: "56317160",
+               cpf: "86000376545",
+               email: "beneditomarcoscavalcanti@vasconcelosdias.com",
+               id: "aaee3d27-f528-488c-a353-83bbe5b02436",
+               inserted_at: nil,
+               name: "Benedito Marcos Enrico Cavalcanti",
+               password: "123456",
+               password_hash: nil,
+               updated_at: nil
+             }
+           } = response
+  end
+
+  test "renders user.json" do
+    user = build(:user)
+
+    response = render(UsersView, "user.json", user: user)
+
+    assert %{
+             user: %Rocketlivery.User{
+               address: "Rua Canto do Vaqueiro, 943",
+               age: 26,
+               cep: "56317160",
+               cpf: "86000376545",
+               email: "beneditomarcoscavalcanti@vasconcelosdias.com",
+               id: "aaee3d27-f528-488c-a353-83bbe5b02436",
+               inserted_at: nil,
+               name: "Benedito Marcos Enrico Cavalcanti",
+               password: "123456",
+               password_hash: nil,
+               updated_at: nil
+             }
+           } = response
+  end
+end
