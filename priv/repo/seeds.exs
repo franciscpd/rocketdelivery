9f05588a52e67d33cc5b87f3f29f8efbc4b4b98a
Francisross Soares de Oliveira <franciscpd@gmail.com>
2022-04-10 09:06:11 -0300

    feat: add initial seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,44 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Rocketlivery.{Item, Order, Repo, User}
+
+user = %User{
+  age: 35,
+  address: "Rua Coronel João Camilo, 542",
+  cep: "14050040",
+  cpf: "22760306070",
+  password: "123456",
+  name: "João da Silva",
+  email: "joao@silva.com"
+}
+
+%User{id: user_id} = Repo.insert!(user)
+
+item1 = %Item{
+  category: :food,
+  description: "banana frita",
+  price: Decimal.new("15.50"),
+  photo: "priv/photos/banana_frita.jpg"
+}
+
+item2 = %Item{
+  category: :food,
+  description: "banana assada",
+  price: Decimal.new("10.50"),
+  photo: "priv/photos/banana_assada.jpg"
+}
+
+Repo.insert!(item1)
+Repo.insert!(item2)
+
+order = %Order{
+  user_id: user_id,
+  items: [item1, item1, item2],
+  address: "Rua Coronel João Camilo, 542",
+  comments: "Por favor, leve em casa",
+  payment_method: :credit_card
+}
+
+Repo.insert!(order)
